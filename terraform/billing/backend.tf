9314f42bdcdcd1ac60e95ffa79e7ed426142e5ef
pmulmi <prannoy.mulmi@gmail.com>
2020-06-20 19:22:31 +0200

    Added backend file

--- terraform/billing/backend.tf
+++ terraform/billing/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-633035071730-eu-west-1"
+    bucket = "terraform-state-bucket-hxmcdpcqyy-eu-west-1"
     region = "eu-west-1"
     key = "aws_billing.json"
     dynamodb_table = "terraform-lock-table"
